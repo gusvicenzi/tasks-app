6ffe9b42f653e44faf315cf16d464f9d12951f7e
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2022-06-14 12:27:12 -0300

    Create TaskList screen

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { ImageBackground, Text, View, StyleSheet } from 'react-native'
 
+import todayImage from '../../assets/imgs/today.jpg'
 export default class TaskList extends Component {
   render() {
     return (
-      <View>
-        <Text>TaskList</Text>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.background}
+          source={todayImage}></ImageBackground>
+        <View style={styles.taskList}>
+          <Text>TaskList</Text>
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+  },
+  background: {
+    flexGrow: 3,
+  },
+  taskList: {
+    flexGrow: 7,
+  },
+})
