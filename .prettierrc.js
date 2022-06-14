5606e3c98411d06172ef8f83c765d68e48b64ac9
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2022-06-14 12:13:13 -0300

    Initial project

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  semi: false,
+  printWidth: 80,
+  parser: 'flow',
+  endOfLine: 'crlf',
+  singleAttributePerLine: true,
+};
