9dc951987bb13b733552f6d250b28bb66bf535da
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2022-06-27 13:57:15 -0300

    Add register new user screen

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -14,8 +14,11 @@ import bgImg from '../../assets/imgs/login.jpg'
 
 export default class Auth extends Component {
   state = {
+    name: '',
     email: '',
     password: '',
+    confirmPassword: '',
+    stageNew: true,
   }
 
   render() {
@@ -25,6 +28,17 @@ export default class Auth extends Component {
         style={styles.background}>
         <Text style={styles.title}>Tasks</Text>
         <View style={styles.formContainer}>
+          <Text style={styles.subtitle}>
+            {this.state.stageNew ? 'Crie a sua conta' : 'Informe seus dados'}
+          </Text>
+          {this.state.stageNew && (
+            <TextInput
+              placeholder="Nome"
+              value={this.state.name}
+              style={styles.input}
+              onChangeText={name => this.setState({ name })}
+            />
+          )}
           <TextInput
             placeholder="E-mail"
             value={this.state.email}
@@ -36,10 +50,24 @@ export default class Auth extends Component {
             value={this.state.password}
             style={styles.input}
             onChangeText={password => this.setState({ password })}
+            secureTextEntry={true}
           />
+          {this.state.stageNew && (
+            <TextInput
+              placeholder="Confirmar senha"
+              value={this.state.confirmPassword}
+              style={styles.input}
+              onChangeText={confirmPassword =>
+                this.setState({ confirmPassword })
+              }
+              secureTextEntry={true}
+            />
+          )}
           <TouchableOpacity>
             <View style={styles.button}>
-              <Text style={styles.buttonText}>Entrar</Text>
+              <Text style={styles.buttonText}>
+                {this.state.stageNew ? 'Registrar' : 'Entrar'}
+              </Text>
             </View>
           </TouchableOpacity>
         </View>
@@ -59,6 +87,13 @@ const styles = StyleSheet.create({
     fontFamily: commonStyles.fontFamily,
     color: commonStyles.colors.secondary,
     fontSize: 70,
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontFamily: commonStyles.fontFamily,
+    color: '#FFF',
+    fontSize: 20,
+    textAlign: 'center',
     marginBottom: 10,
   },
   formContainer: {
